0d3e74785765a9d777a444884dec7bc3bebde454
cjezierski <cjezierski@softia.fr>
2024-01-29 22:44:24 +0100

    plus de div hidden nb_balls

--- background/background.js
+++ background/background.js
@@ -1,21 +1,23 @@
 function click_ball() {
-    nb_balls=document.querySelector("#nb_balls").value;
-    for (i=0;i<nb_balls;i++){
-        const rond=document.createElement("div");
-        rond.className="clickAnim";
-        var width=10+r(100);
-        var animationDuration=200 + r(1500);
-        rond.style.width=`${width}px`;
-        rond.style.height=`${width}px`;
-        rond.style.top= `${window.scrollY + r(window.innerHeight) - 0.5*width}px`;
-        rond.style.left= `${window.scrollX + r(window.innerWidth) - 0.5*width}px`;
-        rond.style.background= `linear-gradient(${r(180)}deg,${get_random_hex_color()},${get_random_hex_color()})`;
-        rond.style.animationDuration= `${animationDuration}ms`;
-        document.body.appendChild(rond);
-        setTimeout(() => {
-            rond.remove()
-        },animationDuration);
-    }
+    chrome.storage.sync.get(["nb_balls"]).then((result) => {
+        let nb_balls = result["nb_balls"]
+        for (let i = 0; i < nb_balls; i++) {
+            const rond = document.createElement("div");
+            rond.className = "clickAnim";
+            var width = 10 + r(100);
+            var animationDuration = 200 + r(1500);
+            rond.style.width = `${width}px`;
+            rond.style.height = `${width}px`;
+            rond.style.top = `${window.scrollY + r(window.innerHeight) - 0.5 * width}px`;
+            rond.style.left = `${window.scrollX + r(window.innerWidth) - 0.5 * width}px`;
+            rond.style.background = `linear-gradient(${r(180)}deg,${get_random_hex_color()},${get_random_hex_color()})`;
+            rond.style.animationDuration = `${animationDuration}ms`;
+            document.body.appendChild(rond);
+            setTimeout(() => {
+                rond.remove()
+            }, animationDuration);
+        }
+    })
 }
 
 function get_random_hex_color() {
@@ -32,28 +34,6 @@ function r(i){
     return Math.floor(Math.random() *i)
 }
 
-function getNb_balls(){
-    chrome.storage.sync.get(["nb_balls"], function(result) {
-        if (result["nb_balls"]==null){
-            document.querySelector("#nb_balls").value=1;
-            click_ball();
-        }
-        else {
-            document.querySelector("#nb_balls").value=result["nb_balls"];
-            click_ball();
-        }
-    });
-}
-
-function create_nb_balls(){
-    nb_balls=document.createElement("div");
-    nb_balls.id="nb_balls";
-    nb_balls.hidden = true;
-    document.body.appendChild(nb_balls);
-}
-
-create_nb_balls();
-
 window.addEventListener("click", () => {
-    getNb_balls();
+    click_ball();
 });
--- pop_up/pop_up.js
+++ pop_up/pop_up.js
@@ -1,24 +1,18 @@
-function setNb_balls(){
+function setNb_balls() {
     var nb_balls=parseInt(document.querySelector("#nb_balls").value);
-    chrome.storage.sync.set({nb_balls:nb_balls}, function() {
-        console.log("la valeur a été changé");
-        chrome.storage.sync.get(["nb_balls"], function(result) {
-            console.log(result);
-        });
-    });
+    chrome.storage.sync.set({nb_balls: nb_balls});
 }
 
-function getNb_balls(){
-    chrome.storage.sync.get(["nb_balls"], function(result) {
+function getNb_balls() {
+    chrome.storage.sync.get(["nb_balls"]).then((result) => {
         if (result["nb_balls"]==null){
             document.querySelector("#nb_balls").value=1;
-        }
-        else {
+            setNb_balls()
+        } else {
             document.querySelector("#nb_balls").value=result["nb_balls"];
         }
     });
 }
 
-getNb_balls();
-
+getNb_balls()
 document.querySelector("#nb_balls").addEventListener("change",setNb_balls);
